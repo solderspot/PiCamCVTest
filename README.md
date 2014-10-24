df04efcfe92d03cb304a338b061693f988f58b45
solderspot <solderspot@gmail.com>
2014-10-23 21:25:27 -0700

    Initital commit

---
+++ README.md
@@ -0,0 +1,69 @@
+PiCamObjDetect Demo
+===================
+
+This is a modified version of a demo created by Chris Cummings.
+
+Original project is at: http://robotblogging.blogspot.co.uk/2013/10/an-efficient-and-simple-c-api-for.html
+
+To Build Project
+================
+
+
+First make sure cmake is installed:
+
+	$ sudo apt-get install cmake
+
+
+Install Userland
+----------------
+
+From your home directory get the latest userland zip ball
+
+	$ wget https://github.com/raspberrypi/userland/zipball/master
+
+You should now have a zip file called 'master'. Unzip it.
+ 
+	$ unzip master
+
+And you should now have a folder called something like 'raspberrypi-userland-xxxxxx'
+
+We need to move and rename this folder to be '/opt/vc/userland-master':
+
+	$ sudo mv raspberrypi-userland-4333d6d /opt/vc/userland-master
+
+Now we build it:
+
+	$ cd /opt/vc/userland-master/
+	$ mkdir build
+	$ cd build
+	$ sudo cmake -DCMAKE_BUILD_TYPE=Release ..
+	$ sudo make
+	$ sudo make install
+
+This part can take a while.
+
+Installing OpenCV
+-----------------
+
+	$ sudo apt-get install libopencv-dev
+
+Install and Build Project
+-------------------------
+
+Move back to you home folder and clone the project:
+
+	$ git clone https://github.com/solderspot/PiCamObjDetect.git
+
+Then build:
+
+	$ cd PiCamObjDetect
+	$ mkdir build
+	$ cd build
+	$ cmake -DCMAKE_BUILD_TYPE=Release ..
+	$ make
+
+And run:
+
+	$ ./PiCamObjDetect 
+
+
---
+++ main.cpp
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "camera.h"
+#include "graphics.h"
+#include <opencv/cv.hpp>
+
+
+#define MAIN_TEXTURE_WIDTH 320
+#define MAIN_TEXTURE_HEIGHT 320
+
+char tmpbuff[MAIN_TEXTURE_WIDTH*MAIN_TEXTURE_HEIGHT*4];
+
+float minTargetRadius = ((float)MAIN_TEXTURE_WIDTH)*0.05;
+bool do_thresholding = true;
+
+using namespace std;
+
+//entry point
+int main(int argc, const char **argv)
+{
+
+	//init graphics and the camera
+	InitGraphics();
+	CCamera* cam = StartCamera(MAIN_TEXTURE_WIDTH, MAIN_TEXTURE_HEIGHT,30, 1, true);
+
+    GfxTexture texture;
+	texture.Create(MAIN_TEXTURE_WIDTH, MAIN_TEXTURE_HEIGHT);
+
+
+	cv::Mat frame;
+
+	printf("Running frame loop\n");
+	while (true)
+	{
+
+		//lock the chosen frame buffer, and copy it directly into the corresponding open gl texture
+		const void* frame_data; int frame_sz;
+		if(cam->BeginReadFrame(0,frame_data,frame_sz))
+		{
+			//if doing argb conversion the frame data will be exactly the right size so just set directly
+			frame = cv::Mat(MAIN_TEXTURE_HEIGHT, MAIN_TEXTURE_WIDTH, CV_8UC4, (void*)frame_data);
+			if (do_thresholding)
+			{
+				cv::cvtColor(frame, frame, CV_RGB2HSV); 
+			}
+
+			cam->EndReadFrame(0);
+
+			if (do_thresholding)
+			{
+				cv::inRange(frame, cv::Scalar(300 / 2, 100, 100), cv::Scalar(360 / 2, 255, 255), frame); 
+				cv::Mat str_el = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
+				morphologyEx(frame, frame, cv::MORPH_OPEN, str_el);
+				morphologyEx(frame, frame, cv::MORPH_CLOSE, str_el);
+
+				std::vector<cv::Point2i> center;
+				std::vector<int> radius;
+
+
+				{
+					std::vector<std::vector<cv::Point> > contours;
+					std::vector<cv::Vec4i> heirarchy;
+
+					cv::findContours( frame.clone(), contours, heirarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE);
+			
+					size_t count = contours.size();
+					
+					for( int i=0; i<count; i++)
+					{
+						cv::Point2f c;
+						float r;
+						cv::minEnclosingCircle( contours[i], c, r);
+						
+						if ( r >= minTargetRadius)
+						{
+							center.push_back(c);
+							radius.push_back(r);
+						}
+					}
+				}
+
+
+				cvtColor( frame, frame, CV_GRAY2RGBA);
+				// draw all the bounding circles
+				
+				size_t count = center.size();
+				
+				for( int i = 0; i < count; i++)
+				{
+					cv::circle(frame, center[i], radius[i], cv::Scalar(255,0,0, 255), 3);
+					printf("circle at %d, %d - radius %d\n", center[i].x, center[i].y, radius[i]);
+				}
+
+			}
+	 }
+
+
+		//begin frame, draw the texture then end frame (the bit of maths just fits the image to the screen while maintaining aspect ratio)
+		BeginFrame();
+		texture.SetPixels(frame.data);
+		float aspect_ratio = float(MAIN_TEXTURE_WIDTH)/float(MAIN_TEXTURE_HEIGHT);
+		float screen_aspect_ratio = 1280.f/720.f;
+		DrawTextureRect(&texture,-aspect_ratio/screen_aspect_ratio,-1.f,aspect_ratio/screen_aspect_ratio,1.f);
+		EndFrame();
+	}
+
+	StopCamera();
+}
